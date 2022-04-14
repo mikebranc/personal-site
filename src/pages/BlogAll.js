7ea55a91c6892aa62a16bb52f983131d637d26f5
Michael Branconier <michaelbranconier@gmail.com>
2022-04-13 17:09:03 -0700

    Created Personal Site

---
+++ src/pages/BlogAll.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function BlogAll(){
+    return(
+        <div>
+            <h1>Dis da blog</h1>
+        </div>
+    )
+}
---
+++ src/pages/BlogDetail.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function BlogDetail(){
+    return(
+        <div>
+            <h1>Dis da blog post</h1>
+        </div>
+    )
+}
