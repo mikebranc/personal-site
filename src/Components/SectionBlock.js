d6162d5629d9d0de46810be9cb1e253b22fdeea2
Michael Branconier <michaelbranconier@gmail.com>
2022-04-18 15:14:36 -0700

    Added About section

---
+++ src/Components/SectionBlock.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SectionBlock(props){
+    return(
+        <div className="sectionBlock">
+            <h1>{props.sectionName}</h1>
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import About from "../Components/About";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
+      <div className="homeWrapper">
+        <About />
+      </div>
     </div>
+
   );
 }
