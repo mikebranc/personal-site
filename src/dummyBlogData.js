a52ef84f0bd10acc2f646604402c2324eaf94bcd
Michael Branconier <michaelbranconier@gmail.com>
2022-04-20 17:34:35 -0700

    Added all blogs page

---
+++ src/dummyBlogData.js
@@ -0,0 +1,21 @@
+export default [{
+    slug: "1",
+    title: "Blog Post 1",
+    date: "4-20-22"
+},
+{
+    slug: "2",
+    title: "Blog Post 2",
+    date: "4-14-22"
+},
+{
+    slug: "3",
+    title: "Blog Post 3",
+    date: "4-12-22"
+},
+{
+    slug: "4",
+    title: "Blog Post 4",
+    date: "4-9-22"
+}
+]
--- src/pages/BlogAll.js
+++ src/pages/BlogAll.js
@@ -1,13 +1,32 @@
 import React from "react"
 import Navbar from "../Components/Navbar";
-import {Outlet} from "react-router-dom"
+import {Outlet} from "react-router-dom";
+import dummyBlogData from "../dummyBlogData";
+import {Link} from "react-router-dom";
+import '../blogAll.css'
+import Footer from "../Components/Footer/Footer";
 
 
 export default function BlogAll(){
+    const blogs = dummyBlogData.map(blog =>{
+        return(
+            <Link to={`/blog/${blog.slug}`} className="post">
+                <div className="postDiv">
+                    <h1>{blog.title}</h1>
+                    <h3>{blog.date}</h3>
+                </div>
+            </Link>
+        )
+    })
     return(
         <div>
             <Navbar />
-            <h1>This is the blog</h1>
+            <div className="postsWrapperOuter">
+                <div className="postsWrapperInner">
+                    {blogs}
+                </div> 
+            </div>
+            <Footer />
         </div>
     )
 }
