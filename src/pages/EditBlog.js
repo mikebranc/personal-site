f17034ebde831d61b2cf3ac3567f6f1d92e8f1fe
Michael Branconier <michaelbranconier@gmail.com>
2022-05-05 13:52:03 -0700

    Created edit blog page and BlogList component

--- src/pages/EditBlog.js
+++ src/pages/EditBlog.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import BlogList from '../Components/BlogList'
 
 
 export default function EditBlog(){
+    const blogWrapper ={
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent:'center',
+    }
+    const nameHeading ={
+        fontFamily: 'Kaushan Script, cursive',
+        textAlign: 'center',
+        paddingTop:'20px',
+        marginBottom:'80px'
+    }
+
     return(
         <div>
-            <h1>This is the edit blog page</h1>
+            <h1 style ={nameHeading}>Michael Branconier</h1>
+            <div style={blogWrapper}>
+                    <BlogList edit={true}/>
+            </div>
         </div>
+
     )
 
 }
