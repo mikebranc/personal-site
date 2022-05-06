b277601758b93cd17421f3e7a794ee9b946293c9
Michael Branconier <michaelbranconier@gmail.com>
2022-05-05 17:02:50 -0700

    added edit navigation on edit blog page

--- src/pages/EditBlog.js
+++ src/pages/EditBlog.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import BlogList from '../Components/BlogList'
+import {Link} from "react-router-dom";
+
 
 
 export default function EditBlog(){
@@ -15,10 +17,17 @@ export default function EditBlog(){
         paddingTop:'20px',
         marginBottom:'80px'
     }
+    const pageWrapper ={
+        display:'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+
+    }
 
     return(
-        <div>
+        <div style = {pageWrapper}>
             <h1 style ={nameHeading}>Michael Branconier</h1>
+            <Link to ="/edit" style={{color:'white', marginBottom:'20px'}}>Back to Edit</Link>
             <div style={blogWrapper}>
                     <BlogList edit={true}/>
             </div>
