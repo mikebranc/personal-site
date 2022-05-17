63003e0d5a5cb76ab9f05024158e60b0dccd7b64
Michael Branconier <michaelbranconier@gmail.com>
2022-05-16 17:54:26 -0700

    added delete helper function and project delete functionality

--- src/pages/EditExperience.js
+++ src/pages/EditExperience.js
@@ -1,11 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import DataList from '../Components/DataList'
-import ExperienceData from '../Components/Experience/ExperienceData'
-import { Link } from 'react-router-dom'
 import "../editSection.css"
-import { firestore } from '../firebase/config'
-import { getFirestoreCollection } from '../dbHelpers'
-import {doc, deleteDoc} from "firebase/firestore"
+import { getFirestoreCollection,deleteFirestoreDocument } from '../dbHelpers'
 
 export default function EditExperience(){
     const [expData, setExpData] = useState()
@@ -17,23 +13,11 @@ export default function EditExperience(){
         getFirestoreCollection("experience", setExpData, setLoading)
       }, [])
     
-     const handleDelete = (event, docId,type) =>{
+    const handleDelete = (event, docId,type) =>{
         event.preventDefault()
         setLoading(true)
-        const deleteEntry = async () =>{
-            try{
-                await deleteDoc(doc(firestore, type,docId))
-                setExpData(oldData =>{
-                    return oldData.filter(curr => curr.id !== docId)
-                })
-                setLoading(false)
-            }
-            catch(error){   
-                throw error.message
-            }
-        }
-        deleteEntry()
-     }
+        deleteFirestoreDocument(docId, setExpData, setLoading, type)
+    }
 return(
     <div className = "pageWrapper">
         <h1 className = "nameHeading">Michael Branconier</h1>
